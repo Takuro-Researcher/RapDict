8f2d97b556a336ef3cd9b868abdef8b600a76d90
Takuro-Researcher <s183369@stn.nagaokaut.ac.jp>
2019-05-26 03:42:47 +0900

    #refs 43 リファクタリングと、100件以下の検索結果に対応するようにした

--- app/src/main/java/com/rapdict/takuro/rapdict/Dict_Activity.kt
+++ app/src/main/java/com/rapdict/takuro/rapdict/Dict_Activity.kt
@@ -24,44 +24,40 @@ class Dict__Activity : AppCompatActivity() {
     @SuppressLint("NewApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //DbAccess関連のインスタンス生成
         helper = SQLiteOpenHelper(applicationContext)
         db = helper!!.writableDatabase
-
         var wordAccess = WordAccess()
 
-
+        //Activityを制御するLayoutインスタンス生成
         val frame_linear_layout = LinearLayout(this)
         frame_linear_layout.layoutParams = LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
         frame_linear_layout.orientation= LinearLayout.VERTICAL
-
         val parent_layout = LinearLayout(this)
         parent_layout.layoutParams = LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
         parent_layout.orientation = LinearLayout.VERTICAL
-
         val over_layout = TableLayout(this)
         over_layout.layoutParams = LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, MATCH_PARENT,0.1F)
-
         val under_layout = TableLayout(this)
         under_layout.layoutParams = LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, MATCH_PARENT,0.1F)
 
-
-        val widgetController = WidgetController(this)
-        val tableRow = arrayOfNulls<TableRow>(100)
-        val checkBox = arrayOfNulls<CheckBox>(100)
-        val question_view = arrayOfNulls<TextView>(100)
-        val answer_view = arrayOfNulls<TextView>(100)
-        val furigana_view = arrayOfNulls<TextView>(100)
-
+        //TableRowの列を制御するLayoutParamsを記述
         val layoutParams3 = TableRow.LayoutParams()
         layoutParams3.weight = 1f
         layoutParams3.setMargins(WidgetController.int_Dp2Px(10f, applicationContext), WidgetController.int_Dp2Px(10f, applicationContext), WidgetController.int_Dp2Px(10f, applicationContext), WidgetController.int_Dp2Px(10f, applicationContext))
 
+        //widegetの設定を行うインスタンスを生成
+        val widgetController = WidgetController(this)
 
+        //一列目、検索に使用するTableRow
         val search_row = TableRow(this)
+        val search_Button = Button(this)
+        search_Button != widgetController.settings(search_Button,5f,5f,5f,5f,8f,"検索開始", CENTER,0)
         val min_spinner = Spinner(this)
         val min = arrayListOf<Int>()
         val max_spinner = Spinner(this)
         val max = arrayListOf<Int>()
+
         for(i in 3..7){
             min.add(i)
         }
@@ -71,7 +67,6 @@ class Dict__Activity : AppCompatActivity() {
         var min_adapter = ArrayAdapter<Int>(this,android.R.layout.simple_spinner_item,min)
         var max_adapter = ArrayAdapter<Int>(this,android.R.layout.simple_spinner_item,max)
         min_spinner.adapter = min_adapter
-
         min_spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             var min_value: Int = 0
             override fun onItemSelected(adapterView: AdapterView<*>, view: View, i: Int, l: Long) {
@@ -86,62 +81,52 @@ class Dict__Activity : AppCompatActivity() {
         }
         max_spinner.adapter = max_adapter
 
-        val search_Button = Button(this)
-        search_Button != widgetController.settings(search_Button,5f,5f,5f,5f,8f,"検索開始", CENTER,0)
         search_row.addView(min_spinner,layoutParams3)
         search_row.addView(max_spinner,layoutParams3)
         search_row.addView(search_Button,layoutParams3)
         search_row.setBackgroundColor(Color.rgb(255,255,200))
         over_layout.addView(search_row)
 
-        var answers:ArrayList<AnswerView> = wordAccess.getAnswers(db!!,0,10,0)
+
+        //2列目
+        val varLayout = TableLayout(this)
+        varLayout.layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+        val layoutParams = TableRow.LayoutParams()
+        layoutParams.weight = 1f
+        val tableRow = arrayOfNulls<TableRow>(100)
+        val checkBox = arrayOfNulls<CheckBox>(100)
+        val question_view = arrayOfNulls<TextView>(100)
+        val answer_view = arrayOfNulls<TextView>(100)
 
         search_Button.setOnClickListener{
             var max =max_spinner.selectedItem as Int
             var min =min_spinner.selectedItem as Int
-            System.out.println("max"+max)
-            System.out.println("min"+min)
-            answers = wordAccess.getAnswers(db!!,min,max,0)
+            val answers = wordAccess.getAnswers(db!!,min,max,0)
+            System.out.println(answers.count())
+            for(i in 0..99){
+                tableRow[i]?.removeAllViews()
+            }
+            for(i in 0..answers.count()-1){
+                tableRow[i]= TableRow(this)
+                tableRow[i]?.setLayoutParams(ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT))
+                answer_view[i] = TextView(this)
+                question_view[i] = TextView(this)
+                checkBox[i] = CheckBox(this)
+                question_view[i] = widgetController.settings(question_view[i]!!, 10f, 10f, 10f, 10f, 6f, answers.get(i).question!!, Gravity.TOP, 1)
+                answer_view[i] = widgetController.settings(answer_view[i]!!, 10f, 10f, 10f, 10f, 6f, answers.get(i).answer!!, Gravity.TOP, 1)
+                tableRow[i]?.addView(question_view[i],layoutParams)
+                tableRow[i]?.addView(answer_view[i],layoutParams)
+                tableRow[i]?.addView(checkBox[i],layoutParams)
+                varLayout.addView(tableRow[i])
+            }
         }
-
-
-
-
-        val varLayout = TableLayout(this)
-        varLayout.layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
-
-
-
-        val layoutParams = TableRow.LayoutParams()
-        layoutParams.weight = 1f
-
-
 
         val scrollView = ScrollView(this)
         scrollView.layoutParams = LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, MATCH_PARENT,0.8F)
-
-        val size = answers!!.count()
-        System.out.println("size"+size)
-
-        for (i in 0..size-1){
-            System.out.println(i)
-            tableRow[i]= TableRow(this)
-            tableRow[i]?.setLayoutParams(ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT))
-
-            answer_view[i] = TextView(this)
-            question_view[i] = TextView(this)
-            checkBox[i] = CheckBox(this)
-
-            question_view[i] = widgetController.settings(question_view[i]!!, 10f, 10f, 10f, 10f, 6f, answers.get(i).question!!, Gravity.TOP, 1)
-            answer_view[i] = widgetController.settings(answer_view[i]!!, 10f, 10f, 10f, 10f, 6f, answers.get(i).answer!!, Gravity.TOP, 1)
-
-            tableRow[i]?.addView(question_view[i],layoutParams)
-            tableRow[i]?.addView(answer_view[i],layoutParams)
-            tableRow[i]?.addView(checkBox[i],layoutParams)
-            varLayout.addView(tableRow[i])
-        }
         scrollView.addView(varLayout)
 
+
+        //3列目
         val underRow = TableRow(this)
         val back_Button = Button(this)
         val next_Button = Button(this)
@@ -155,7 +140,7 @@ class Dict__Activity : AppCompatActivity() {
         underRow.setBackgroundColor(Color.rgb(147,112,219))
         under_layout.addView(underRow)
 
-
+        //上中下段、全て適用させる。
         parent_layout.addView(over_layout)
         parent_layout.addView(scrollView)
         parent_layout.addView(under_layout)
