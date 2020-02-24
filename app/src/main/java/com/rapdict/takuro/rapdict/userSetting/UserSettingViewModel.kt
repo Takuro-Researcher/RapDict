4b20c02dfdafe65ae64697f303ea03448906e755
Takuro-Researcher <s183369@stn.nagaokaut.ac.jp>
2020-02-24 23:18:30 +0900

    refs #145 Targetの値をViewModelで管理

--- app/src/main/java/com/rapdict/takuro/rapdict/userSetting/UserSettingViewModel.kt
+++ app/src/main/java/com/rapdict/takuro/rapdict/userSetting/UserSettingViewModel.kt
@@ -5,6 +5,7 @@ import android.content.SharedPreferences
 import android.preference.PreferenceManager
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
+import com.rapdict.takuro.rapdict.Common.CommonTool
 import com.rapdict.takuro.rapdict.Common.SpfCommon
 import com.rapdict.takuro.rapdict.R
 
@@ -12,16 +13,20 @@ class UserSettingViewModel(application: Application) : AndroidViewModel(applicat
 
     //監視対象のLiveData
     var userName: MutableLiveData<String> = MutableLiveData()
+    var numberArray: MutableLiveData<ArrayList<Int>> = MutableLiveData()
     val spf: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplication())
 
+    private val common = CommonTool()
 
     //ViewModel初期化時にロード
     init {
         loadUserData()
     }
     private fun loadUserData(){
-        val app = getApplication<Application>()
+        val numArrayList = common.makeNumArray(1,15)
         userName.value = spf.getString("userName","")
+        numberArray.value = numArrayList
+
     }
 
 }
