c8187caf821b8570eff9131c2fb0e3b586110c27
Takuro-Researcher <s183369@stn.nagaokaut.ac.jp>
2020-02-26 02:14:13 +0900

    refs #148 ViewModel疎通

--- app/src/main/java/com/rapdict/takuro/rapdict/gameSetting/GameSettingFragment.kt
+++ app/src/main/java/com/rapdict/takuro/rapdict/gameSetting/GameSettingFragment.kt
@@ -10,40 +10,34 @@ import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import com.rapdict.takuro.rapdict.R
+import com.rapdict.takuro.rapdict.databinding.FragmentGameSettingBinding
 import com.rapdict.takuro.rapdict.game.GameActivity
 import kotlinx.android.synthetic.main.fragment_game_setting.*
+import org.koin.android.viewmodel.ext.android.viewModel
 
 class GameSettingFragment : androidx.fragment.app.Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-    private var listener: OnFragmentInteractionListener? = null
-
+    private var binding:FragmentGameSettingBinding ?= null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
         }
     }
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game_setting, container, false)
+        binding = FragmentGameSettingBinding.inflate(inflater, container,false)
+        binding!!.lifecycleOwner = this
+        return binding!!.root
     }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val gameSettingViewModel:GameSettingViewModel by viewModel()
+        binding?.settingData  = gameSettingViewModel
+    }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val question = make_list_num(10,30,10)
-        val time = make_list_num(3,15,3)
-        val min = make_list_num(3,8,1)
-        val max =make_list_num(4,14,1)
-        val ret = make_list_num(1,4,1)
-
-        val questionAdapter= ArrayAdapter(context!!,android.R.layout.simple_spinner_item,question)
-        val timeAdapter = ArrayAdapter(context!!,android.R.layout.simple_spinner_item,time)
-        val minAdapter = ArrayAdapter(context!!,android.R.layout.simple_spinner_item,min)
-        val returnAdapter = ArrayAdapter(context!!,android.R.layout.simple_spinner_item,ret)
-        val maxAdapter = ArrayAdapter(context!!,android.R.layout.simple_spinner_item,max)
 
         val questionSpinner = question_spinner as Spinner
         val timeSpinner = time_spinner as Spinner
@@ -51,22 +45,13 @@ class GameSettingFragment : androidx.fragment.app.Fragment() {
         val maxSpiner = max_spinner as Spinner
         val returnSpinner = return_spinner as Spinner
 
-        questionSpinner.adapter = questionAdapter
-        timeSpinner.adapter = timeAdapter
-        minSpinner.adapter = minAdapter
-        maxSpiner.adapter =maxAdapter
-        returnSpinner.adapter =returnAdapter
+
 
         //最小文字>最大文字とならないように、スピナーの値を順次変更する
         minSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            var min_value: Int = 0
+
             override fun onItemSelected(adapterView: AdapterView<*>, view: View, i: Int, l: Long) {
-                val spinner = adapterView as Spinner
-                min_value = spinner.selectedItem as Int
-                maxAdapter.clear();
-                for (f in min_value + 1..10) {
-                    maxAdapter.add(f)
-                }
+
             }
             override fun onNothingSelected(adapterView: AdapterView<*>) {}
         }
@@ -80,31 +65,6 @@ class GameSettingFragment : androidx.fragment.app.Fragment() {
             intent.putExtra("RETURN",returnSpinner.selectedItem as Int)
             startActivity(intent)
         }
-
-
-
-    }
-    fun make_list_num(start:Int,end:Int,inc:Int):ArrayList<Int>{
-        val List = ArrayList<Int>()
-        for (i in start..end step inc){
-            List.add(i)
-        }
-        return List
     }
 
-    interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        fun onFragmentInteraction(uri: Uri)
-    }
-
-    companion object {
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance() =
-                GameSettingFragment().apply {
-                    arguments = Bundle().apply {
-
-                    }
-                }
-    }
 }
---
+++ app/src/main/java/com/rapdict/takuro/rapdict/gameSetting/GameSettingViewModel.kt
@@ -0,0 +1,30 @@
+package com.rapdict.takuro.rapdict.gameSetting
+
+import android.app.Application
+
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.rapdict.takuro.rapdict.Common.CommonTool
+
+class GameSettingViewModel(application: Application) : AndroidViewModel(application) {
+
+    //監視対象のLiveData
+    var questionArray: MutableLiveData<ArrayList<Int>> = MutableLiveData()
+    var minArray: MutableLiveData<ArrayList<Int>> = MutableLiveData()
+    var maxArray: MutableLiveData<ArrayList<Int>> = MutableLiveData()
+    var returnArray: MutableLiveData<ArrayList<Int>> = MutableLiveData()
+    var timeArray: MutableLiveData<ArrayList<Int>> = MutableLiveData()
+    private var commonTool:CommonTool = CommonTool()
+
+    //ViewModel初期化時にロード
+    init {
+        loadUserData()
+    }
+    private fun loadUserData(){
+        questionArray.value = commonTool.makeNumArray(10,30,10)
+        minArray.value = commonTool.makeNumArray(3,7)
+        maxArray.value = commonTool.makeNumArray(4,10)
+        returnArray.value = commonTool.makeNumArray(1,4)
+        timeArray.value = commonTool.makeNumArray(3,10)
+    }
+}
