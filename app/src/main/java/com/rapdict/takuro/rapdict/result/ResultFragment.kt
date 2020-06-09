52c34baf2fbeabfff61b52a51ba87c26d07891ca
Takuro-Researcher <takuro.developer@gmail.com>
2020-06-10 01:52:20 +0900

    refs #176 デザインを作った

--- app/src/main/java/com/rapdict/takuro/rapdict/result/ResultFragment.kt
+++ app/src/main/java/com/rapdict/takuro/rapdict/result/ResultFragment.kt
@@ -21,21 +21,16 @@ private const val ARG_PARAM2 = "param2"
 
 class ResultFragment : androidx.fragment.app.Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
     private var listener: OnFragmentInteractionListener? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_result, container, false)
     }
 
@@ -47,7 +42,7 @@ class ResultFragment : androidx.fragment.app.Fragment() {
         val typeToken = object : TypeToken<Array<AnswerData>>() {}
         val list = Gson().fromJson<Array<AnswerData>>(answerList, typeToken.type)
 
-        val listView = rhyme_list_view
+        val listView = result_rhyme_list_view
         val answerView = AnswerView()
         listView.adapter = ResultListAdapter(context!!,list)
         list?.forEach {
