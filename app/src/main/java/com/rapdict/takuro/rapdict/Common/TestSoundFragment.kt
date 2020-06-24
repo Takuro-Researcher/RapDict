0a601fab84847c5cedb2d9f1c77d31d2525b2a84
Takuro-Researcher <takuro.developer@gmail.com>
2020-06-24 13:34:16 +0900

    refs #193 音声認識動作確認。手動でのループまで

--- app/src/main/java/com/rapdict/takuro/rapdict/Common/TestSoundFragment.kt
+++ app/src/main/java/com/rapdict/takuro/rapdict/Common/TestSoundFragment.kt
@@ -6,45 +6,57 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.rapdict.takuro.rapdict.R
-import com.rapdict.takuro.rapdict.databinding.FragmentUserExpBinding
-import com.rapdict.takuro.rapdict.exp.UserExpViewModel
 import kotlinx.android.synthetic.main.fragment_test_sound.*
-import kotlinx.android.synthetic.main.fragment_user_exp.*
-import org.koin.android.viewmodel.ext.android.viewModel
+
 
 class TestSoundFragment : androidx.fragment.app.Fragment() {
 
-
+    var mediaPlayer: MediaPlayer ?= null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        var mediaPlayer: MediaPlayer? = MediaPlayer.create(context, R.raw.beat_97_dronly)
+        mediaPlayer = MediaPlayer.create(activity, R.raw.beat_97)
         test_sound_button.setOnClickListener {
-            mediaPlayer?.seekTo(0)
-            mediaPlayer?.start()
-            mediaPlayer?.seekTo(0)
-            mediaPlayer?.start()
-            mediaPlayer?.seekTo(0)
-            mediaPlayer?.start()
+            mediaPlayer?.let { it1 -> onCompletion(it1) }
+        }
+        //
+        stop_button.setOnClickListener {
+            mediaPlayer?.pause()
+        }
+        var counter: Int = 0
+        //音声停止時の挙動。疑似的
+        mediaPlayer?.setOnCompletionListener {
+            counter++
+            if(counter == 4){
+                mediaPlayer?.pause()
+                counter = 0
+            }else{
+                onCompletion(it)
+            }
+
 
         }
+    }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer?.stop()
+        mediaPlayer?.reset();
+        mediaPlayer?.release();
     }
     fun onCompletion(player: MediaPlayer){
         player.seekTo(0)
         player.start()
-        System.out.println("ああ")
+        System.out.println("あ")
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         super.onCreateView(inflater, container, savedInstanceState)
         return inflater.inflate(R.layout.fragment_test_sound, container, false);
-
-
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
