33a0d2859c366a57cee61220210f7f557264147a
Takuro-Researcher <takuro.developer@gmail.com>
2020-06-26 04:12:23 +0900

    refs #195 新たなデザインとspinner用の問題テキストリストを用意

--- app/src/main/java/com/rapdict/takuro/rapdict/result/ResultListViewModel.kt
+++ app/src/main/java/com/rapdict/takuro/rapdict/result/ResultListViewModel.kt
@@ -4,6 +4,7 @@ import android.app.Application
 
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
+import com.rapdict.takuro.rapdict.Word
 import com.rapdict.takuro.rapdict.model.Answer
 
 import sample.intent.AnswerData
@@ -14,14 +15,20 @@ class ResultListViewModel(application: Application) : AndroidViewModel(applicati
     var textList   = mutableListOf<MutableLiveData<String>>()
     var questionList = mutableListOf<MutableLiveData<String>>()
     var checkedList = mutableListOf<MutableLiveData<Boolean>>()
+    var wordTextArray: MutableLiveData<ArrayList<String>> = MutableLiveData()
 
 
-    fun draw(answerList:ArrayList<Answer>) {
+    fun draw(answerList:ArrayList<Answer>,wordList:ArrayList<Word>) {
         answerList.forEach { answer ->
             questionList.add(MutableLiveData<String>().apply { value = answer.question })
             textList.add(MutableLiveData<String>().apply { value = answer.answer })
             checkedList.add(MutableLiveData<Boolean>().apply { value = false })
         }
+        var arraytextList = ArrayList<String>()
+        wordList.forEach {
+            arraytextList.add(it.word)
+        }
+        wordTextArray.value = arraytextList
     }
 
 }
