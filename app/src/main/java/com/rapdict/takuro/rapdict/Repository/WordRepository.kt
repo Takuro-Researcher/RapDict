f69d2859b16fca3bb304a8f4569804f5d07da822
Takuro-Researcher <takuro.developer@gmail.com>
2021-01-14 17:25:49 +0900

    MyDictMakeQuestion部分変更。 極力UI側の処理以外ViewModel内に書き込まないようにした

---
+++ app/src/main/java/com/rapdict/takuro/rapdict/Repository/WordRepository.kt
@@ -0,0 +1,17 @@
+package com.rapdict.takuro.rapdict.Repository
+import android.content.Context
+import com.rapdict.takuro.rapdict.Word
+import com.rapdict.takuro.rapdict.database.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class WordRepository(val context: Context){
+    private val wordDao: WordDao by lazy { RapDataBase.getInstance(context).wordDao()}
+    suspend fun saveWords(words: List<Word>) {
+        withContext(Dispatchers.IO) {
+            words.forEach {
+                wordDao.insert(it)
+            }
+        }
+    }
+}
--- app/src/main/java/com/rapdict/takuro/rapdict/myDict/MyDictMakeQuestionFragment.kt
+++ app/src/main/java/com/rapdict/takuro/rapdict/myDict/MyDictMakeQuestionFragment.kt
@@ -9,10 +9,10 @@ import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProviders
 import com.rapdict.takuro.rapdict.Common.App.Companion.db
 import com.rapdict.takuro.rapdict.R
 import com.rapdict.takuro.rapdict.Word
+import com.rapdict.takuro.rapdict.databinding.FragmentMydictQuestionMakeBinding
 import com.rapdict.takuro.rapdict.main.MainActivity
 import kotlinx.android.synthetic.main.fragment_mydict1.*
 import kotlinx.android.synthetic.main.fragment_mydict_question_make.*
@@ -20,77 +20,71 @@ import kotlinx.coroutines.runBlocking
 
 
 class MyDictMakeQuestionFragment : Fragment() {
+
+    private var binding: FragmentMydictQuestionMakeBinding? = null
     private val myDictChoiceViewModel :MyDictChoiceViewModel by activityViewModels()
+    private val myDictMakeQuestionViewModel: MyDictMakeQuestionViewModel by viewModels()
+    private lateinit var saveDialog: AlertDialog.Builder
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        val intent  = Intent(requireActivity(), MainActivity::class.java)
+        val uid = myDictChoiceViewModel.db_uid.value
+
+        saveDialog = AlertDialog.Builder(requireActivity()).apply {
+            setCancelable(false)
+            setTitle("")
+            setMessage("")
+            setPositiveButton("OK") { _, _ ->
+                myDictMakeQuestionViewModel.registerQuestion(uid!!)
+                myDictMakeQuestionViewModel.clear()
+                startActivity(intent)
+            }
+            setNegativeButton("NO", null)
+        }
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_mydict_question_make, container, false)
+        binding = FragmentMydictQuestionMakeBinding.inflate(inflater, container, false)
+        binding!!.lifecycleOwner = this
+        return binding!!.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        // TODO: Use the ViewModel
-        val questionListViewModel: QuestionListViewModel by viewModels()
-        val adapter = QuestionListAdapter(questionListViewModel, this)
 
+        val adapter = MyDictMakeQuestionListAdapter(myDictMakeQuestionViewModel, this)
+        binding?.data = myDictMakeQuestionViewModel
 
         QuestionRecyclerView.adapter = adapter
         QuestionRecyclerView.layoutManager = androidx.recyclerview.widget.LinearLayoutManager(context, androidx.recyclerview.widget.LinearLayoutManager.VERTICAL, false)
-        val recomIntent = Intent(requireActivity(), MainActivity::class.java)
 
         add_question_button.setOnClickListener {
-            questionListViewModel.addCard()
-            adapter.notifyItemInserted(adapter.itemCount)
+            myDictMakeQuestionViewModel.addCard()
         }
 
+        myDictMakeQuestionViewModel.myDictMakeWords.observe(viewLifecycleOwner, androidx.lifecycle.Observer {
+            adapter.submitList(it)
+        })
+
+
+
         register_question_button.setOnClickListener {
-            var db_name = ""
-            runBlocking {
-                val dao = db.mydictDao()
-                val data = dao.findOneByIds(myDictChoiceViewModel.db_uid.value!!)
-                db_name = data.name!!
+            // セーブする前に使用するDBと、ふりがなや文字の状況をまとめる
+            val uid = myDictChoiceViewModel.db_uid.value
+            myDictMakeQuestionViewModel.updateStatus(uid!!)
+            val dialogTitle:String = "問題保存【"+myDictMakeQuestionViewModel.dbName +"】"
+            var dialogMessage:String = myDictMakeQuestionViewModel.myDictMakeWords.value?.size.toString() +"個、保存する\n"
+            if(myDictMakeQuestionViewModel.isFuriganaEmpty){
+                dialogMessage += "フリガナが空の場合、言葉がそのままフリガナとして登録されます \n"
             }
-
-            val q_list = questionListViewModel.questionList
-            val f_list = questionListViewModel.furiganaList
-            val word_list = ArrayList<Word>()
-            var furiganaAttention = false
-            var furiganaMsg = ""
-            f_list.zip(q_list).forEach {
-                var furigana = it.first.value
-                var question = it.second.value
-                if (question!!.isEmpty()) {
-                    return@forEach
-                }
-                if (furigana!!.isEmpty()) {
-                    furiganaAttention = true
-                    furigana = question
-                }
-                word_list.add(Word(0, furigana, question, furigana.length, myDictChoiceViewModel.db_uid.value))
+            if(myDictMakeQuestionViewModel.isinCompleteFlag){
+                dialogMessage += "言葉が空のものは保存されません\n"
             }
-
-
-            if (furiganaAttention) {
-                furiganaMsg = "読み方が空のものがあります。空の場合、好きな言葉を読み方に適用します"
-            }
-
-            val saveDialog = AlertDialog.Builder(requireActivity()).apply {
-                setCancelable(false)
-                setTitle("問題保存【" + db_name + "】")
-                setMessage(word_list.size.toString() + "個、自分の問題として保存\n好きな言葉が空のものは保存されません\n" + furiganaMsg + "\n※画面移動します")
-                setPositiveButton("OK", { _, _ ->
-                    runBlocking {
-                        val dao = db.wordDao()
-                        word_list.forEach { dao.insert(it) }
-                        myDictChoiceViewModel.count_visible()
-                    }
-                    questionListViewModel.clear()
-
-                    val fragment = parentFragment as MyDictFragment
-                    fragment.mydict_tab_layout?.getTabAt(1)?.select()
-                })
-                setNegativeButton("NO", null)
-            }
+            dialogMessage += "※保存後、画面移動します"
+            saveDialog.setTitle(dialogTitle)
+            saveDialog.setMessage(dialogMessage)
             saveDialog.show()
         }
     }
